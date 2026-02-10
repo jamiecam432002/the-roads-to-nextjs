3e551533ac5343d767390932ecd2790eebf6c84b
James Cameron <jamiecam43@gmail.com>
2026-02-10 11:22:02 -0500

    playing around with caching strategies

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,22 +1,21 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
-  "rootMainFilesTree": {},
+globalThis.__BUILD_MANIFEST = {
   "pages": {
     "/_app": []
   },
-  "ampFirstPages": []
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/33c6e7ef9aa43899.js",
+    "static/chunks/806bdb8e4a6a9b95.js",
+    "static/chunks/38eb66a58ed5ffd7.js",
+    "static/chunks/e1140cc73d7c6ca2.js",
+    "static/chunks/turbopack-1533596fb4a454f9.js"
+  ]
 };
-self.__BUILD_MANIFEST.lowPriorityFiles = [
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
 ];
--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,26 +1,35 @@
-import { notFound } from "next/navigation";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { notFound } from 'next/navigation';
+import { TicketItem } from '@/features/ticket/components/ticket-item';
+import { getTicket } from '@/features/ticket/queries/get-ticket';
+import { getTickets } from '@/features/ticket/queries/get-tickets';
 
 type TicketPageProps = {
-  params: Promise<{
-    ticketId: string;
-  }>;
+	params: Promise<{
+		ticketId: string;
+	}>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const { ticketId } = await params;
-  const ticket = await getTicket(ticketId);
+	const { ticketId } = await params;
+	const ticket = await getTicket(ticketId);
 
-  if (!ticket) {
-    notFound();
-  }
+	if (!ticket) {
+		notFound();
+	}
 
-  return (
-    <div className="flex justify-center animate-fade-from-top">
-      <TicketItem ticket={ticket} isDetail />
-    </div>
-  );
+	return (
+		<div className='flex justify-center animate-fade-from-top'>
+			<TicketItem ticket={ticket} isDetail />
+		</div>
+	);
 };
+
+export async function generateStaticParams() {
+	const tickets = await getTickets();
+
+	return tickets.map((ticket) => ({
+		ticketId: ticket.id,
+	}));
+}
 
 export default TicketPage;
