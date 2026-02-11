34080d4fcf87f5c564cc80efa03f00816ed2a2ad
James Cameron <jamiecam43@gmail.com>
2026-02-11 11:11:01 -0500

    added create ticket form

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,19 @@ import { Suspense } from 'react';
 import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
 import { TicketList } from '@/features/ticket/components/ticket-list';
+import TicketCreateForm from '@/features/ticket/components/ticket-create-form';
+import CardCompact from '@/components/card-compact';
 
 const TicketsPage = async () => {
 	return (
 		<div className='flex-1 flex flex-col gap-y-8'>
 			<Heading title='Tickets' description='All your tickets at one place' />
+			<CardCompact
+				title='Create Ticket'
+				description='A new ticket will be created'
+				content={<TicketCreateForm />}
+				className='w-full max-w-[420px] self-center'
+			/>
 
 			<Suspense fallback={<Spinner />}>
 				<TicketList />
